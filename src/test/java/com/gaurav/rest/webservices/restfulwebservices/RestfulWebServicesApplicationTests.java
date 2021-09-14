7a50b735296402351d0a3fb52df0e6d31f419fa1
gaurav <graghava@uci.edu>
2021-09-13 20:02:41 -0400

    base commit

---
+++ src/test/java/com/gaurav/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaurav.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
